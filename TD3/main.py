843533ba4bd2f7b405a928b9719dfdda163a37e7
Peter Ma <map3@uci.edu>
2026-05-25 23:00:11 -0700

    Lookback to 5

--- TD3/main.py
+++ TD3/main.py
@@ -28,7 +28,7 @@ def main(
     buffer_size: int = 262_144,
     batch_size: int = 1024,
     learning_starts: int = 100_000,
-    n_step: int = 15,
+    n_step: int = 5,
     utd: float = 1.0,
     gamma: float = 0.99,
     tau: float = 0.005,
